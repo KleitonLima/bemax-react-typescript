c7505ad7d233be7d837ccc1197b9d81384f6cf97
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-19 17:30:59 -0300

    feat(App): change for render Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
-import Login from "./pages/Login";
+import Router from "./router";
 
 function App() {
   return (
     <div>
       <Toaster />
-      <Login />
+      <Router />
     </div>
   );
 }
