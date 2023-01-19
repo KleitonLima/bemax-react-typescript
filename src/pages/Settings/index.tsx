f20a742f7339a4623a12abe422725d846bd44190
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-19 17:34:13 -0300

    feat(Settings): create component Settings

---
+++ src/pages/Settings/index.tsx
@@ -0,0 +1,16 @@
+import * as Styled from "./styles";
+
+const Settings = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>+</h2>
+          <p>Adicionar quiosque</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
