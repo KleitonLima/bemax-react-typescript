949ee8c715f244a37f1798fa8d546a1494b76263
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-19 14:53:13 -0300

    feat(App): includes Toaster and Login components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Toaster />
+      <Login />
+    </div>
+  );
 }
 
 export default App;
