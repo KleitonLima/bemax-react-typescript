653b0d41d5d48bd14a3a9d26bc097c07c69f353d
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-19 17:32:37 -0300

    feat(router): create Route for component Settings

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,13 @@
-import {  Route } from "react-router";
+import { Route } from "react-router";
 import { Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Settings from "./pages/Settings";
 
 const Router = () => {
-
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/settings/kiosks" element={<Settings/>} />
     </Routes>
   );
 };
