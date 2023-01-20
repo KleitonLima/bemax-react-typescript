5c0fbd81da93345db28d5802eb4c27d2b7aec1a4
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-20 17:07:40 -0300

    style(MenuHeader): change VW to % & remove margin

--- src/components/MenuHeader/styles.ts
+++ src/components/MenuHeader/styles.ts
@@ -2,17 +2,17 @@ import styled from "styled-components";
 
 export const BackContainer = styled.header`
   display: flex;
-  width: 100vw;
+  width: 100%;
   align-items: center;
   justify-content: center;
+  box-sizing: border-box;
 `;
 
 export const Container = styled.div`
-  width: 98vw;
+  width: 97%;
   height: 10vh;
   border-radius: 0 0 16px 16px;
   box-shadow: 0px 8px 8px 0px #0004;
-  margin: 0 16px 0 16px;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -25,4 +25,3 @@ export const Container = styled.div`
     padding-right: 32px;
   }
 `;
-
