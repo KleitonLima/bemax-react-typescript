2178989ec2561a08254ca8232034695b8d9d1b3c
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-20 17:08:51 -0300

    feat(KioskCheckoutModal): added mocked data

--- src/components/KioskCheckoutModal/index.tsx
+++ src/components/KioskCheckoutModal/index.tsx
@@ -1,7 +1,32 @@
 import * as Styled from "./styles";
 
-const KioskCheckoutModal = () => {
-  return <div></div>;
+interface KioskCheckoutModalProps {
+  handleShowKioskModal: () => void;
+}
+const KioskCheckoutModal = ({
+  handleShowKioskModal,
+}: KioskCheckoutModalProps) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="./quiosque-icon.png" alt="" width="50px" />
+        </div>
+        <div>
+          <h3>Quiosque 1</h3>
+          <h3>R$30,00</h3>
+          <input type="date" />
+          <button>Alugar</button>
+        </div>
+        <div>
+          <h4>Mais informações</h4>
+          <p>Entrada: 06h00</p>
+          <p>Saída: 18h00</p>
+        </div>
+        <p onClick={handleShowKioskModal}>X</p>
+      </div>
+    </div>
+  );
 };
 
 export default KioskCheckoutModal;
