a79cdfa27c0bc14c5cbff8eea6469b3456c43e14
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-20 17:05:52 -0300

    feat(KioskCard): ready card with mocked data

--- src/components/KioskCard/styles.ts
+++ src/components/KioskCard/styles.ts
@@ -1,1 +1,31 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 300px;
+  height: 100px;
+  display: flex;
+  justify-content: space-around;
+  cursor: pointer;
+  box-shadow: 0px -4px 4px 0px #0004;
+  border-radius: 8px;
+
+  :hover {
+    background-color: #86e1fa;
+    transform: scale(1.05);
+    transition: all, 0.5s;
+    box-shadow: 0 0 2px 0;
+
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    h3,
+    p {
+      margin: 8px 0 8px 0;
+    }
+  }
+`;
