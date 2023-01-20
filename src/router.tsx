de3f8076fa59e3c48b7695424a425c429018e6c4
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-20 16:29:29 -0300

    feat(router): add route homes

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,15 @@
 import { Route } from "react-router";
 import { Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Settings from "./pages/Settings";
 
 const Router = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/settings/kiosks" element={<Settings/>} />
+      <Route path="/settings/kiosks" element={<Settings />} />
     </Routes>
   );
 };
