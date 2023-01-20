4e235a6a14495f72c828575c134c605a1d13c1f3
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-19 23:18:50 -0300

    feat(MenuHeader): create & implemented

---
+++ src/components/MenuHeader/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const BackContainer = styled.header`
+  display: flex;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  width: 98vw;
+  height: 10vh;
+  border-radius: 0 0 16px 16px;
+  box-shadow: 0px 8px 8px 0px #0004;
+  margin: 0 16px 0 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fafafa;
+
+  img {
+    padding-left: 8px;
+  }
+  div {
+    padding-right: 32px;
+  }
+`;
+
