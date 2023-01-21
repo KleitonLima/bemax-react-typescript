b11ad68c90f78321d1b18016d671fbbac97d6028
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-20 21:24:04 -0300

    style(globalStyles): new styles added

--- src/services/styles/globalStyles.ts
+++ src/services/styles/globalStyles.ts
@@ -13,6 +13,8 @@ export const Button = styled.button<GlobalStyleProps>`
     border: none;
     font-size: medium;
     font-weight: 700;
+    cursor: pointer;
+    margin: 8px;
     font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
       sans-serif;
 
@@ -28,4 +30,31 @@ export const Button = styled.button<GlobalStyleProps>`
       box-shadow: 0 0 2px 0;
     }
   `}
+`;
+
+export const ModalOverlay = styled.section`
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0009;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Content = styled.main`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 48px;
+  padding: 32px;
+  box-sizing: border-box;
 `;
